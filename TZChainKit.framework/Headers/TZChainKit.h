33a557b84cab7867ead5894b082ea6c7c510d9a9
tangzeng <zengzeng_tang@kingdee.com>
2020-05-11 16:32:22 +0800

    ddd

---
+++ TZChainKit.framework/Headers/TZChainKit.h
@@ -0,0 +1,20 @@
+//
+//  TZChainKit.h
+//  TZChainKit
+//
+//  Created by tangzeng on 11.5.20.
+//  Copyright © 2020 zeng tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TZChainKit.
+FOUNDATION_EXPORT double TZChainKitVersionNumber;
+
+//! Project version string for TZChainKit.
+FOUNDATION_EXPORT const unsigned char TZChainKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TZChainKit/PublicHeader.h>
+
+
+#import "TZUtil.h"
---
+++ TZChainKit.framework/Headers/TZUtil.h
@@ -0,0 +1,17 @@
+//
+//  TZUtil.h
+//  TZChainKit
+//
+//  Created by tangzeng on 11.5.20.
+//  Copyright © 2020 zeng tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TZUtil : NSObject
++ (void)postLog;
+@end
+
+NS_ASSUME_NONNULL_END
